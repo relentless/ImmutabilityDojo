f157e3219380e2cca6a20d197e86a513bb73ccfc
Grant Crofton <grantac@hotmail.com>
2016-11-28 11:50:18 +0000

    Added first bit of greeting kata

---
+++ GreetingKata/Greetings/Friend.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Greetings
+{
+    public class Friend
+    {
+        public static string Greet(params string[] names) {
+
+            var notNullNames = names ?? new string[] { "Friend" };
+            var joinedNames = notNullNames.Length > 1 ? JoinLastTwoNames(notNullNames) : notNullNames;
+            var flattenedNames = string.Join(", ", joinedNames);
+
+            if (AllUppercase(flattenedNames)) {
+                return string.Format("HELLO {0}!", flattenedNames);
+            }
+            else {
+                return string.Format("Hello, {0}", (flattenedNames ?? "Friend"));
+            }
+        }
+
+        private static IEnumerable<string> JoinLastTwoNames(string[] notNullNames) {
+            return notNullNames.ToList()
+                .Take(notNullNames.Length - 2)
+                .Concat(new List<string> { string.Format("{0} and {1}",
+                                        notNullNames[notNullNames.Length - 2],
+                                        notNullNames[notNullNames.Length - 1]) });
+        }
+
+        private static bool AllUppercase(string text) {
+            return text != null 
+                && text.ToUpper() == text;
+        }
+    }
+}
---
+++ GreetingKata/Greetings/Friend_Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Greetings {
+    [TestFixture]
+    public class Friend_Tests {
+        [Test]
+        public void Greet_GivenName_SaysHello() {
+            var greeting = Friend.Greet("Bob");
+            Assert.That(greeting, Is.EqualTo("Hello, Bob"));
+        }
+
+        [Test]
+        public void Greet_GivenNull_SaysHelloWithStandIn() {
+            var greeting = Friend.Greet(null);
+            Assert.That(greeting, Is.EqualTo("Hello, Friend"));
+        }
+
+        [Test]
+        public void Greet_NameShouted_ShoutsBack() {
+            var greeting = Friend.Greet("BOB");
+            Assert.That(greeting, Is.EqualTo("HELLO BOB!"));
+        }
+
+        [Test]
+        public void Greet_TwoNames_GreetsBoth() {
+            var greeting = Friend.Greet("Bob", "Sue");
+            Assert.That(greeting, Is.EqualTo("Hello, Bob and Sue"));
+        }
+
+        [Test]
+        public void Greet_MultipleNames_GreetsAll() {
+            var greeting = Friend.Greet("Rita", "Sue", "Bob");
+            Assert.That(greeting, Is.EqualTo("Hello, Rita, Sue and Bob"));
+        }
+    }
+}
