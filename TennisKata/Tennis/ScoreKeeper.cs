7402aa2e736e44e0b819d02a1d94b7182cc05708
Grant Crofton <grantac@hotmail.com>
2016-11-28 12:35:28 +0000

    Added tennis kata

---
+++ TennisKata/Tennis/ScoreKeeper.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+
+namespace Tennis
+{
+    public enum Player { A, B };
+
+    public class ScoreKeeper {
+        private readonly ImmutableList<Player> _pointsWon;
+
+        private readonly ImmutableDictionary<int, string> scoreDescriptions =
+            new Dictionary<int, string> {
+                {0, "Love"},
+                {1, "Fifteen"},
+                {2, "Thirty"},
+                {3, "Forty"}
+            }.ToImmutableDictionary();
+
+        public ScoreKeeper(ImmutableList<Player> pointsWon) {
+            _pointsWon = pointsWon;
+        }
+
+        public ScoreKeeper(): this(ImmutableList.Create<Player>()) {}
+
+        public ScoreKeeper BallWon(Player winner) {
+            return new ScoreKeeper(_pointsWon.Add(winner));
+        }
+
+        public string Score {
+            get {
+
+                var playerAPoints = _pointsWon.Where(player => player == Player.A).Count();
+                var playerBPoints = _pointsWon.Where(player => player == Player.B).Count();
+
+                return string.Format("{0}-{1}", scoreDescriptions[playerAPoints], scoreDescriptions[playerBPoints]);
+            }
+        }
+    }
+}
---
+++ TennisKata/Tennis/ScoreKeeper_Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tennis {
+    [TestFixture]
+    public class ScoreKeeper_Tests {
+        [Test]
+        public void score_starts_at_love_love() {
+            var scorer = new ScoreKeeper();
+            Assert.That(scorer.Score, Is.EqualTo("Love-Love"));
+        }
+
+        [Test]
+        public void player_A_scores_fifteen_love() {
+            var scorer = new ScoreKeeper();
+            var firstScore = scorer.BallWon(Player.A);
+            Assert.That(firstScore.Score, Is.EqualTo("Fifteen-Love"));
+        }
+
+        [Test]
+        public void player_B_scores_love_fifteen() {
+            var scorer = new ScoreKeeper();
+            var firstScore = scorer.BallWon(Player.B);
+            Assert.That(firstScore.Score, Is.EqualTo("Love-Fifteen"));
+        }
+
+        [Test]
+        public void second_score_is_thirty() {
+            var scorer = new ScoreKeeper();
+            var firstScore = scorer.BallWon(Player.A);
+            var secondScore = firstScore.BallWon(Player.A);
+            Assert.That(secondScore.Score, Is.EqualTo("Thirty-Love"));
+        }
+    }
+}
